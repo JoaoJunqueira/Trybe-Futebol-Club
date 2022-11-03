1f0aacebd67391eb7793dbb94eaa20e63b92b94c
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-11-03 09:49:09 -0300

    fix: package.npm lint problem

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,46 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Match extends Model {
+  // declare <campo>: <tipo>;
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Match.init({
+  // ... Campos
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+  },
+  awayTeam: {
+    type: INTEGER,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+  },
+  inProgress: {
+    type: BOOLEAN,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Match;
