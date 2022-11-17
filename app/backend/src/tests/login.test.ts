7c780075123cde3c83c0171f66b3698616dfc649
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-11-17 20:39:22 -0300

    feat: sucessfull login requests tests

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,6 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
-// import jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcryptjs';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -9,6 +10,9 @@ import { app } from '../app';
 
 import { Response } from 'superagent';
 import User from '../database/models/UserModel';
+import LoginController from '../database/controllers/LoginController';
+import LoginService from '../database/services/LoginService';
+import IMessage from '../database/interfaces/IMessage';
 // import IUser from '../database/interfaces/IUser';
 
 chai.use(chaiHttp);
@@ -36,7 +40,69 @@ describe('Testando as models', () => {
         password: 'string',
       })
       expect(chaiHttpResponse.status).to.equal(401)
+    })
+    it('deve retornar um body { message: "Incorrect email or password" }', async () => {
+      const chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'string',
+        password: 'string',
+      })
       expect(chaiHttpResponse.body).to.deep.equal({ message: 'Incorrect email or password' })
+    })
+  })
+});
+describe('Testando /login post', () => {
+  describe('Quando o usuário está cadastrado', () => {
+    const user = {
+      email: 'admin@admin.com',
+      // password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      password: 'secret_admin'
+    }
+    beforeEach(() => {
+      sinon.stub(User, 'findAll').resolves([user] as User[])
+      sinon.stub(bcrypt, 'compare').resolves(true)
+    })
+    afterEach(() => sinon.restore())
+    it('Deve retornar o token', async () => {
+      const chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(user)
+      expect(chaiHttpResponse.body).to.have.own.property('token')
+    })
+  })
+});
+describe('Testando /login/validate get', () => {
+  describe('Quando o usuário está cadastrado', () => {
+    const user = {
+      username: 'Admin',
+      role: 'admin',
+      email: 'admin@admin.com',
+      password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+        // senha: secret_admin
+    }
+    const payload = {
+      email: 'admin@admin.com',
+      password: 'secret_admin',
+    }
+    const header = {
+      authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+      .eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ
+      .XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc`
+    }
+    beforeEach(() => {
+      sinon.stub(jwt, 'verify').resolves(payload)
+      sinon.stub(User, 'findAll').resolves([user] as User[])
+    })
+    afterEach(() => sinon.restore())
+    it('Deve retornar status 200', async () => {
+      const chaiHttpResponse = await chai
+      .request(app)
+      .get('/login/validate')
+      .send({ header })
+      expect(chaiHttpResponse.status).to.equal(200)
     })
   })
 });
